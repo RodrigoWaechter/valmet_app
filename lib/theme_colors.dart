42453efd33524f7a781504d9bf5139b578e9767b
Lucas <souzadasilva101lucas@gmail.com>
2025-11-20 17:54:38 -0300

    implementação da tela 'Sobre'

--- lib/theme_colors.dart
+++ lib/theme_colors.dart
@@ -5,3 +5,7 @@ const Color kValmetDarkGrey = Color(0xFF5A5A5A);
 const Color kValmetBeige = Color(0xFFF5F0E8);
 const Color kValmetButtonGrey = Color(0xFFE0E0E0);
 const Color kValmetTextDark = Color(0xFF333333);
+
+// cores para a tela 'Sobre'
+const Color kValmetDarkBackground = Color (0xFF767676);
+const Color kValmetSubtleDivider = Color (0xFF989898);
